c79227b0fdb8f33e908ca4541e58a22bc8618a57
Yume Webs <71258059+yumewebs@users.noreply.github.com>
2022-03-23 23:03:02 +0100

    Update binance_key.py

--- binance_key.py
+++ binance_key.py
@@ -1,3 +1,3 @@
 
-BinanceKey = {'api_key': 'B1LTSsAORJkEV86Fx2Y74HDYHn9NSPMpvTXuz7bnTiBoDrZqmjYnDm6EnjD6Uuq9',
-    'api_secret': 'rZWvZAfEUJ20ZJEVUA0KzUb3rdESJhUpGmECwD1c6MZCJLfYtOhZjW18odTKAEgw'}
+BinanceKey = {'api_key': '',
+    'api_secret': ''}
